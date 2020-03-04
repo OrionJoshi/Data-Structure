eecd8701200688e4bbd309ae4568c66722ccf146
khageshwor <khageshwarjoshi03@gmail.com>
2020-03-04 08:52:49 -0800

    Array logical question added

---
+++ Array/check identity matrix.cpp
@@ -0,0 +1,44 @@
+/* C Program to check Matrix is an Identity Matrix or Not */
+ 
+#include<stdio.h>
+#include<conio.h>
+ 
+int main(void)
+{
+ 	int i, j, rows, columns, a[10][10], Flag = 1;
+  
+ 	printf("\n Please Enter Number of rows and columns  :  ");
+ 	scanf("%d %d", &i, &j);
+ 
+ 	printf("\n Please Enter the Matrix Elements \n");
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0; columns < j; columns++)
+    	{
+      		scanf("%d", &a[rows][columns]);
+    	}
+  	}
+     
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0; columns < j; columns++)
+    	{
+    		if(a[rows][columns] != 1 && a[columns][rows] != 0)
+    		{
+    			Flag = 0;
+    			break;
+			}
+   	 	}
+  	}
+  	if(Flag == 1)
+  	{
+  		printf("\n The Matrix that you entered is an Identity Matrix ");
+	}
+	else
+	{
+		printf("\n The Matrix that you entered is Not an Identity Matrix ");
+	}
+  	
+  	getch();
+ 	return 0;
+}
